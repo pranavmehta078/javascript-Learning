f5d674a77ee407f33a889a10d8126895035d84f3
Pranav Mehta <pranavmehta078@gmail.com>
2026-04-22 10:16:34 +0530

    First commit for day 1

---
+++ javascript-learning/day1.js
@@ -0,0 +1,12 @@
+//Use of Let and const
+
+//let is used where you can change the value later
+
+let name = "pranav";
+name = "Mehta";
+
+console.log(name);
+
+//const value you can not change after define
+const experience = '10';
+console.log(experience);
