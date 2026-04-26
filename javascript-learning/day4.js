cf20d64c72c80f99bd76a9b1b9148b5b79806a3a
Pranav Mehta <pranav.mehta@CP-DOMAIN.LOCAL>
2026-04-26 18:47:20 +0530

    day4 practical

---
+++ javascript-learning/day4.js
@@ -0,0 +1,17 @@
+
+//for loop
+
+for(let i = 0; i<=5; i++){
+    console.log(i);
+}
+
+
+//while loop
+let j = 0;
+
+while(j <= 5){
+
+    console.log(j);
+    j++;
+
+}
---
+++ practice/printnumber.js
@@ -0,0 +1,13 @@
+//print 1 to 10 number
+for(i =0; i <=10; i++){
+    console.log(i);
+}
+
+//Print Even number
+
+for(j=0; j<=20; j++){
+
+    if(j % 2 == 0){
+        console.log("Event number is :" + j);
+    }
+}
